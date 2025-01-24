c851c1224c6e1931df72e1d9bcd0afd77bb23fb0
Farouq Taiwo <148275215+KRD66@users.noreply.github.com>
2025-01-24 21:52:13 +0100

    Update bank_account

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,25 +1,16 @@
-class BankAccount :
+class BankAccount:
     def __init__(self, initial_balance=0):
-        # Initialize the account balance, defaulting to zero
-         self.account_balance = initial_balance
+        self.account_balance = initial_balance
+
     def deposit(self, amount):
-        """Deposit specified amount ijto the account."""
-        if amount > 0:  # Ensure the deposit is a positive number
-            self.account_balance += amount
-            print(f"Deposited: ${amount:.2f}")
-        else:
-            print("Deposit amount must be positive.")
+        self.account_balance += amount
 
     def withdraw(self, amount):
-        """Withdraw the specified amount from the account."""
-        if amount > 0 and amount <= self.account_balance:
+        if self.account_balance >= amount:
             self.account_balance -= amount
-            print(f"Withdrew: ${amount:.2f}")
             return True
         else:
-            print("Insufficient funds or invalid withdrawal amount.")
             return False
 
     def display_balance(self):
-        """Display the current account balance."""
-        print(f"Current balance: ${self.account_balance:.2f}")
+        print(f"Current Balance: ${self.account_balance:.2f}")
